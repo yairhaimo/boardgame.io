24ca0048de11bad56587f8410bc8bfe68e23b73a
Nicolo Davis <nicolodavis@google.com>
2018-06-10 12:53:49 +0800

    mock out context in unit test

--- src/ui/ui.test.js
+++ src/ui/ui.test.js
@@ -7,23 +7,39 @@
  */
 
 import React from 'react';
-import { UI } from './ui';
-import { Card } from './card';
-import { Deck } from './deck';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+beforeEach(() => {
+  jest.resetModules();
+});
+
+const CreateWithContext = context => {
+  jest.doMock('./ui-context', () => {
+    return {
+      Provider: props => props.children,
+      Consumer: props => props.children(context),
+    };
+  });
+  const UI = require('./ui').UI;
+  const Card = require('./card').Card;
+  const Deck = require('./deck').Deck;
+  return { UI, Card, Deck };
+};
+
 describe('basic', () => {
+  const { UI, Card, Deck } = CreateWithContext({});
+
   let root;
   beforeEach(() => {
-    root = Enzyme.shallow(
+    root = Enzyme.mount(
       <UI>
-        <Deck>
-          <Card />
+        <Deck id="1">
+          <Card id="1" />
         </Deck>
-        <Card />
+        <Card id="2" />
       </UI>
     );
   });
